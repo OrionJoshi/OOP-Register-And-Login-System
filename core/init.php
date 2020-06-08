75e1377375a0adf444753f7064262c70fda2304f
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-08 12:25:42 +0545

    Config global variable defined

---
+++ core/init.php
@@ -0,0 +1,23 @@
+<?php
+
+    session_start();
+
+    $GLOBALS['config'] = array(
+        'mysql' => array(
+            'host' => '127.0.0.1',
+            'username' => 'root',
+            'password' => '',
+            'db' => 'lr'
+
+        ),
+        'remember' => array(
+            'cookie_name' => 'hash',
+            'cookie_expiry' => 604800
+        ),
+        'session' => array(
+            'session_name' => 'user'
+        )
+
+    );
+
+?>
