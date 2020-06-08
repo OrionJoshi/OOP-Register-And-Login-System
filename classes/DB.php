a926d6648aba61565d64a9e8710342a78553b3d5
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-08 18:08:33 +0545

    Query and error function add

--- classes/DB.php
+++ classes/DB.php
@@ -9,7 +9,7 @@
         
         private function __construct() {
             try {
-                $this->pdo = new PDO('mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'), Config::get('mysql/username'), Config::get('mysql/password'));
+                $this->_pdo = new PDO('mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'), Config::get('mysql/username'), Config::get('mysql/password'));
             } catch(PDOException $e) {
                 die($e->getMessage());
             }
@@ -20,6 +20,31 @@
                 self::$_instance = new DB();
             }
             return self::$_instance;
+        }
+
+        public function query($sql, $params = array()) {
+            $this->_error = false;
+            if($this->_query = $this->_pdo->prepare($sql)) {
+                $x = 1;
+                if(count($params)) {
+                    foreach($params as $param) {
+                        $this->_query->bindValue($x, $param);
+                        $x++;
+                    }
+                }
+
+                if($this->_query->execute()) {
+                    $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+                    $this->_count = $this->_query->rowCount();
+                } else {
+                    $this->_error = true;
+                }
+            }
+            return $this;
+        }
+
+        public function error() {
+            return $this->_error;
         }
     }
 ?>
