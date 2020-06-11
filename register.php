ae20c7cc594ab858de784d9020cf492abb39bc0c
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-11 19:23:56 +0545

    Add Register form

---
+++ register.php
@@ -0,0 +1,21 @@
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username", id="username" value="" autocomplete="off">
+    </div>
+
+    <div class="field">
+        <label for="password">Choose a password</label>
+        <input type="password" name="password" id="password">
+    </div>
+    <div class="field">
+        <label for="password_again">Enter your password again</label>
+        <input type="password" name="password_again" id="password_again">
+    </div>
+    <div class="field">
+        <label for="name">Enter your name</label>
+        <input type="text" name="name" id="name">
+    </div>
+
+    <input type="submit" value="Register">
+</form>
