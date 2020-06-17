5f890016ab41e6f2dd735da95b42c51fd98d277c
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-17 18:01:49 +0545

    Add functions to unset and return session value

--- classes/Session.php
+++ classes/Session.php
@@ -8,6 +8,16 @@
         public static function put($name, $value) {
             return $_SESSION[$name] = $value;
         }
+
+        public static function get($name) {
+            return $_SESSION[$name];
+        }
+
+        public static function delete($name) {
+            if(self::exists($name)) {
+                unset($_SESSION[$name]);
+            }
+        }
     }
 
 ?>
