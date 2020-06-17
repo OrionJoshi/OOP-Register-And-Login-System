90ef33df852429475f31c93d34d52be7b4a95b65
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-17 15:41:44 +0545

    Add function to return form data

--- classes/Input.php
+++ classes/Input.php
@@ -15,7 +15,15 @@
             }
         }
 
-       
+        public static function get($item) {
+            
+            if(isset($_POST[$item])) {
+                return $_POST[$item];
+            } else if(isset($_GET[$item])) {
+                return $_GET[$item];
+            }
+            return '';
+        }
     }
 
 ?>
