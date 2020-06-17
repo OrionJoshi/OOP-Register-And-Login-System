f3758c3182cbf975fea929a9235fac33916f1e66
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-17 17:52:14 +0545

    Add function to check session name exists or not

--- classes/Session.php
+++ classes/Session.php
@@ -1,5 +1,10 @@
 <?php
     class Session {
+
+        public static function exists($name) {
+            return (isset($_SESSION[$name])) ? true : false;
+        }
+
         public static function put($name, $value) {
             return $_SESSION[$name] = $value;
         }
