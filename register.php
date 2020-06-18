5789c27e65d2ebd2026ec74d8d2f72f8dd54ee02
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-18 15:24:58 +0545

    Register user into database

--- register.php
+++ register.php
@@ -27,7 +27,23 @@
             ));
 
             if($validation->passed()) {
-                echo "Passed";
+                $user = new User();
+
+                $salt = Hash::salt(32);
+
+                try {
+                    $user->create(array(
+                        'username' => Input::get('username'),
+                        'password' => Hash::make(Input::get('password'), $salt),
+                        'salt' => $salt,
+                        'name' => Input::get('name'),
+                        'joined' => date('Y-m-d H:i:s'),
+                        'group' => 1
+                    ));
+                } catch(Exception $e) {
+                    die($e->getMessage());
+                }
+
             } else {
                 foreach($validation->errors() as $error) {
                     echo $error . '<br>';
