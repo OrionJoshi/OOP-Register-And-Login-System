2446e2321af92462964f3e5e83ad8833ccf22fcf
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-18 16:23:17 +0545

    Add login form validation

--- login.php
+++ login.php
@@ -1,3 +1,24 @@
+<?php
+    require_once 'core/init.php';
+    if(Input::exists()) {
+        if(Token::check(Input::get('token'))) {
+
+            $validate = new Validate();
+            $validation = $validate->check($_POST, array(
+                'username' => array('required' => true),
+                'password' => array('required' => true)
+            ));
+
+            if($validation->passed()) {
+                
+            } else {
+                foreach($validation->errors() as $error) {
+                    echo $error . '<br>';
+                }
+            }
+        }
+    }
+?>
 <form action="" method='post'>
     <div class="field">
         <label for="username">Username</label>
@@ -10,5 +31,5 @@
     </div>
 
     <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
-    <input type=" submit" value="Log in">
+    <input type="submit" value="Log in">
 </form>
