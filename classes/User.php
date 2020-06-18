4ef5a3789514695f4b1ffa20b5873ef6b5127e80
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-18 16:28:28 +0545

    Error for insert user data

--- classes/User.php
+++ classes/User.php
@@ -5,6 +5,12 @@
         public function __construct($user = null) {
             $this->_db = DB::getInstance();
         }
+
+        public function create($fields = array()) {
+            if(!$this->_db->insert('users', $fields)) {
+                throw new Exception('There was a problem creating an account.');
+            }
+        }
     }
 
 ?>
