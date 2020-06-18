d6f2747bdf8fa12b785fcff8d4c63219c55e8efe
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-18 15:03:28 +0545

    Add function to generate salt

--- classes/Hash.php
+++ classes/Hash.php
@@ -3,5 +3,10 @@
         public static function make($string, $salt = '') {
             return hash('sha256', $string . $salt);
         }
+
+        public static function salt($length) {
+            return mcrypt_create_iv($length);
+        }
+        
     }
 ?>
