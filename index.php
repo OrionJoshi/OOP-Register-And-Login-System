10d6e98d298d96e9a3e61261196bc05bc9d6ec02
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-19 17:50:44 +0545

    register and login link add

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
     require_once 'core/init.php';
 
-    $user = DB::getInstance()->update('users',2, array(
-        'password' => 'passDon',
-        'name' => 'Don'
-    ));
+   if(Session::exists('home')) {
+       echo '<p>' . Session::flash('home') . '</p>';
+   }
 
-    if($user) {
-        echo "success";
-    }else{
-        echo "Failure";
-    }
+   $user = new User();
+   if($user->isLoggedIn()) {
+?>
+    <p>Hello <a href="#"><?php echo escape($user->data()->username); ?></a>!</p>
+
+    <ul>
+        <li><a href="logout.php">Log Out</a></li>
+    </ul>
+<?php
+   } else {
+       echo '<p>You need to <a href="login.php">log in</a> or <a href="register.php">register</a></p>';
+   }
